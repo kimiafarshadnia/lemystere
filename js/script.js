42e98f4c72cb1734a3789d8d79adfb9499dc26b5
kimia <farshadniakimia@gmail.com>
2022-11-27 19:18:23 +0330

    complete perfume page

--- js/script.js
+++ js/script.js
@@ -3,4 +3,45 @@ $(document).ready(function() {
     $("#mobile-menu-button").click(function() {
         $("#mobile-menu").toggleClass("hidden");
     });
+
+    $("#perfumeLeft").click(function() {
+        $("#modalLeft").toggleClass("hidden");
+    });
+
+    $("#perfumeRight").click(function() {
+        $("#modalRight").toggleClass("hidden");
+    });
+
+    $("#perfumeCenter").click(function() {
+        $("#modalCenter").toggleClass("hidden");
+    });
+
+    $(".modal-overlay").click(function() {
+        $(".Modal").addClass("hidden");
+    })
+
+    // var openmodal = document.querySelectorAll('.modal-open')
+    // for (var i = 0; i < openmodal.length; i++) {
+    //     openmodal[i].addEventListener('click', function(event) {
+    //         event.preventDefault()
+    //         toggleModal()
+    //     })
+    // }
+
+    // const overlay = document.querySelector('.modal-overlay')
+    // overlay.addEventListener('click', toggleModal)
+
+
+    // function toggleModal() {
+    //     const modal = document.querySelector('.modal')
+    //     modal.classList.toggle('opacity-0')
+    //     modal.classList.toggle('pointer-events-none')
+    // }
+
+    // modal center
+
+    // Get the modal
+
+
+
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
     content: ["./pages/index.html", "./pages/perfume.html", "./pages/skincare.html"],
 
     theme: {
-        extend: {},
+        extend: {
+            backgroundImage: {
+                'modalLeft': "url('../images/Perfume Trends to Look Out for This Summer.png')",
+                'modalCenter': "url('../images/Santal Royal Guerlain.jpg')",
+                'modalRight': "url('../images/aboutone.jpg')",
+            },
+        },
     },
     plugins: [
         require('tailwindcss'),
