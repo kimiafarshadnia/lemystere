357e0eb7ef06a9f33f4b353256a73465c2498545
kimia <farshadniakimia@gmail.com>
2022-11-30 11:17:45 +0330

    add box shadow around center perfume

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,17 @@ module.exports = {
     content: ["./pages/index.html", "./pages/perfume.html", "./pages/skincare.html"],
 
     theme: {
+
         extend: {
             backgroundImage: {
                 'modalLeft': "url('../images/Perfume Trends to Look Out for This Summer.png')",
                 'modalCenter': "url('../images/Santal Royal Guerlain.jpg')",
                 'modalRight': "url('../images/aboutone.jpg')",
             },
+
+            dropShadow: {
+                'red': '0 35px 35px rgb(255 33 33 / 15%)',
+            }
         },
     },
     plugins: [
